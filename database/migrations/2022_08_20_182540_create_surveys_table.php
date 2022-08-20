641fd84515eb8ecb383ffc640553b470cf568335
Francisco Alvarez <faove@hotmail.com>
2022-08-20 16:55:28 -0300

    MVC y Migraciones

---
+++ database/migrations/2022_08_20_182540_create_surveys_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->id();            
+            $table->string('title_survey');
+            $table->string('category_survey');
+            $table->float('quantity_survey');
+            $table->boolean('active_survey');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surveys');
+    }
+};
